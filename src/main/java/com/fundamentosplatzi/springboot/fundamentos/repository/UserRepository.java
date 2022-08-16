e75619d40f4b24319023ede456dc41d441603e11
Nanchz <hernanrmz04@gmail.com>
2022-08-15 19:22:33 -0500

    Adition pageable and postman collection

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/repository/UserRepository.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/repository/UserRepository.java
@@ -6,6 +6,7 @@ import net.bytebuddy.asm.Advice;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
     @Query("Select u from User u WHERE u.email=?1")
     Optional<User> findByUserEmail(String email);
@@ -44,5 +45,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
             )
     Optional<UserDto> getAllByBirthDateAndMail(@Param("parametroFecha")LocalDate date,
                                                @Param("parametroEmail")String Email);
+
+    List<User> findAll();
 
 }
